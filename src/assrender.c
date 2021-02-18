a2484270f28f92c2547de999a333f5d24d1bca6d
Pinterf <pinterfer@gmail.com>
2021-02-18 12:08:27 +0100

    Bump version to 0.31

--- src/assrender.c
+++ src/assrender.c
@@ -315,6 +315,6 @@ const char* AVSC_CC avisynth_c_plugin_init(AVS_ScriptEnvironment* env)
                      "[sar]f[top]i[bottom]i[left]i[right]i[charset]s"
                      "[debuglevel]i[fontdir]s[srt_font]s[colorspace]s",
                      assrender_create, 0);
-    return "AssRender 0.30: draws text subtitles better and faster than ever before";
+    return "AssRender 0.31: draws text subtitles better and faster than ever before";
     // for Windows DLL : update version in .rc as well
 }
