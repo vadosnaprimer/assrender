b5f5f8e6e845b81604decfc4cc342807d5fe53ed
Pinterf <pinterfer@gmail.com>
2021-03-01 11:32:50 +0100

    Bump version to 0.34

--- src/assrender.c
+++ src/assrender.c
@@ -324,5 +324,5 @@ const char* AVSC_CC avisynth_c_plugin_init(AVS_ScriptEnvironment* env)
                      "[sar]f[top]i[bottom]i[left]i[right]i[charset]s"
                      "[debuglevel]i[fontdir]s[srt_font]s[colorspace]s",
                      assrender_create, 0);
-    return "AssRender 0.33: draws text subtitles better and faster than ever before";
+    return "AssRender: draws text subtitles better and faster than ever before";
 }
